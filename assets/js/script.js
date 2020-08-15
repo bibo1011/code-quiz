5500cca9b64272496d238047998eb1a897484f50
Blue Abad <bibo1011@gmail.com>
2020-08-15 01:02:50 -0700

    add five questions as variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,109 @@
 // assignment codes
-var startButton = document.getElementById('start')
-var showHighScores = document.getElementById('high-scores')
-var timerEl = document.getElementById('time')
+var startButton = document.getElementById('start');
+var showHighScores = document.getElementById('high-scores');
+var timerEl = document.getElementById('time');
+var question = document.getElementById("question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var title = document.getElementById("title");
+var instruction = document.getElementById("instruction");
+var home = document.getElementById('home');
+
+// create questions
+var questions = [
+    { 
+        question: "What is the correct syntax to close the <title> element?",
+        choiceA: "</title>",
+        choiceB: "<!tittle>",
+        choiceC: "<?title>",
+        choiceD: "<endtitle>",
+        correct: "A"
+    },{ 
+        question: "Which statement correctly stores data into the Web Storage API?",
+        choiceA: "localStorage.getItem('lunch', 'sandwich')",
+        choiceB: "localStorage.setItem('lunch', 'sandwich')",
+        choiceC: "getItem.localStorage'lunch', 'sandwich')",
+        choiceD: "setItem.localStorage('lunch', 'sandwich')",
+        correct: "B"
+    },{
+        question: "How do you leave hidden comments in HTML code?",
+        choiceA: "/* */",
+        choiceB: "<!-- -->",
+        choiceC: "{{ }}",
+        choiceD: "//",
+        correct: "B"
+    },{
+        question: "Which of these values is NOT considered false",
+        choiceA: "0",
+        choiceB: "'0'",
+        choiceC: "null",
+        choiceD: "''",
+        correct: "B"
+    },{
+        question: "What are the CLI commands for Creating a new directory?",
+        choiceA: "mkdir <directory name>",
+        choiceB: "cd <directory name>",
+        choiceC: "touch <file name>",
+        choiceD: "rm <file name>",
+        correct: "A"
+    }
+]
+// for (var i = 0; i < questions.length; i++){
+//     console.log(questions[i].question);
+
+//     var answer = (questions[i].question);
+//     console.log(answer);
+
+//     if (answer === questions[i].correct) {
+//         alert("correct");
+//     }
+
+// }
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+
+function homeDisplay(){
+    title.innerHTML = `<h1>Coding Quiz Challenge</h1>`;
+    instruction.innerHTML = '<p>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>';
+    instruction.style.margin = "auto";
+}
+
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+        
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+}
+
+// function renderProgress(){
+//     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+//         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+//     }
+// }
+
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    // quiz.style.display = "block";
+    // renderProgress();
+    // renderCounter();
+    // TIMER = setInterval(renderCounter,1000);
+}
+    
+homeDisplay();
+
 
 // timer for 75 seconds
-
 function countdown() {
     var timeLeft = 75;
-    // timerEl.textContent
         
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function() {
@@ -22,9 +118,21 @@ function countdown() {
         }
         else {
             clearInterval(timeInterval);
-            displayMessage();
         }
     }, 1000);
-}
+    home.style.display = "none";
+
+    startQuiz();
+    
+    
+    renderQuestion();
+    // renderProgress();
+
+    
+
+    
         
+        
+    
+}
 startButton.onclick = countdown
